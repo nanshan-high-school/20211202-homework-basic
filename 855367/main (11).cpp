e396737f0c3bcb8cadad577ebe67f6536a3fa61a
Barry20070221 <a855367@stdmail.nssh.ntpc.edu.tw>
2021-12-08 21:57:34 +0800

    855367 315 35 賴柏睿 檔案分類

---
+++ 855367/main (11).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  int file;
+  int files[100] = {0};
+  int num[100] = {0};
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> file;
+    files[i] = int((file % 1000) / 10);
+    
+  }
+  for(int i = 0; i < n; i++ ){
+    num[files[i]] += 1;
+  }
+  for(int i = 0; i < 100; i++){
+    if(num[i] != 0){
+      cout << i << " " << num [i] << "\n";
+    }
+  }
+ 
+} 
