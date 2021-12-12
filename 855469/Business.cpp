1ff4d74764adfc1feca2e81b595fcbd8a98ddd68
ya <hans960317+github@gmail.com>
2021-12-12 12:51:51 +0800

    31346 855469 齊志成

--- 855469/Business.cpp
+++ 855469/Business.cpp
@@ -8,38 +8,37 @@ int main() {
     shops[i] = people;
     cin >> people;
   }
+  
   int N;
   cin >> N;
   for(int i = 0; i < N; i++){
     int delta_people[30]={0};
+    
     for(int j = 0; shops[j] != 0; j++){
       if(j == 0 ){
         if(shops[0] >  shops[1]){
-          delta_people[1] += shops[0] * 0.1;;
+          delta_people[1] += shops[0] * 0.1;
         }
-
       }else if(shops[j + 1] != 0){
         if(shops[j] >  shops[j - 1]){
           delta_people[j - 1] += shops[j] * 0.05;
         }
-
         if(shops[j] >  shops[j + 1]){
           delta_people[j + 1] += shops[j] * 0.05;
         }
-
       }else{
         if(shops[j] >  shops[j - 1]){
           delta_people[j - 1] += shops[j] * 0.1;
         }
       }
+    }
     
-      for(int j = 0; shops[j] != 0; j++){
+    for(int j = 0; shops[j] != 0; j++){
         shops[j] += delta_people[j];
-      }
-    } 
-  } 
+    }
+  }
+  
   for(int i = 0; shops[i] != 0; i++){
     cout << shops[i] << " ";
   }
 }
-
