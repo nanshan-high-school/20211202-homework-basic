483b2b18ac59084788aa92615462e49fb6b86f8c
Henry13y <a855440@stdmail.nssh.ntpc.edu.tw>
2021-12-22 19:53:11 +0800

    316 855440 周旻誼 20211202 基礎班作業

---
+++ 855440/cards.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int c[N];
+    for (int i = 0; i < N; i++) { 
+        cin >> c[i];    
+    } 
+    int c_n[N];
+    for (int k = 0; k < M; k++) {
+        for (int j = 0; j < (N / 2); j++) {
+            c_n[j * 2] = c[j];
+            c_n[(j * 2) + 1] = c[j + (N / 2)];
+        } 
+        for (int l = 0; l < N; l++) {
+            c[l] = c_n[l];
+        }
+    }
+    for (int m = 0; m < N; m++) {
+        cout << c[m] << " ";
+    }
+}
