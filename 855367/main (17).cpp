bb3cebab6f132bee0f919a01264d1c83db5a9f46
Barry20070221 <a855367@stdmail.nssh.ntpc.edu.tw>
2021-12-22 21:23:58 +0800

    855367 313 35 賴柏睿 files cards business

--- 855367/main (17).cpp
+++ 855367/main (17).cpp
@@ -9,21 +9,23 @@ int main() {
   
   int bef[300] = {0};
   int now[300] = {0};
-  int c[300] = {0};
-  int d[300] = {0};
   for(int k = 0; k < a; k++){
     cin >> bef[k];    
   }
-  for(int j = 0; j < a / 2; j++){
-    c[j] = bef[j];
-  }
-  for(int l = 0; l < a / 2; l++){
-    d[l] = bef[l + 3];
-  }
+  
   for(int i = 1; i <= b; i++ ){
-    for(int g = 0; g < a / 2; g++){
-      now[g] = c[g]
+    int j = 0;
+    for(int g = 0; g < a ; g+=2){
+      now[g] =bef[j];
+      now[g+1] = bef[j + a/2];
+      j += 1;
     }
+    for(int h = 0;h < a;h++ ){
+      bef[h] = now[h];
+    }
+  }
+  for(int e = 0;e < a; e++){
+      cout << now[e];
+  }
 }
 
-}
